eade549a27c750df71abd7a1536d5fc54274865f
harun <kamandeharun99@mail.com>
2024-04-15 23:57:27 +0300

    sboots

---
+++ src/main/java/com/Farmed/demo/Greeting.java
@@ -0,0 +1,28 @@
+package com.Farmed.demo;
+
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long setId(long id) {
+        return this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String setContent(String content) {
+        return this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/Farmed/demo/GreetingController.java
@@ -0,0 +1,13 @@
+package com.Farmed.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public Greeting greeting() {
+        return new Greeting(1, "Hello World!");
+    }
+}
